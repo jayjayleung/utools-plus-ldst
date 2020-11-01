85348615c832f01f118516dd18bd8ac27fe64e2e
JayJay <741940091@qq.com>
2020-11-01 23:52:21 +0800

    jsoup

--- ldst/src/main/java/com/jayjay/ldst/httpclient/Test.java
+++ ldst/src/main/java/com/jayjay/ldst/httpclient/Test.java
@@ -1,17 +1,16 @@
 package com.jayjay.ldst.httpclient;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import com.jayjay.ldst.util.HttpClientUtil;
+import com.jayjay.ldst.util.JsoupUtil;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Copyright © 2020-ESS
@@ -24,31 +23,93 @@ import java.io.IOException;
  */
 public class Test {
     //声明需要爬取的网址
-    static String URL="https://www.mzitu.com/page/2/";
+    static String URL="https://www.mzitu.com/page/1/";
+//    static String URL="https://www.google.com/";
 
     //主函数入口
     public static void main(String args[]) throws IOException {
         String result = HttpClientUtil.get(URL);
 
-//        System.out.println(result);
-        Document doc = Jsoup.parse(result);
-        Elements postlist = doc.select(".postlist");
-//        System.out.println(postlist.html());
-        Elements li = postlist.select("li a");
-//        li.forEach(item-> System.out.println(item.select("a")));
-        System.out.println(li.select("a").get(0).attr("href"));
-        String detail = HttpClientUtil.get(li.select("a").get(0).attr("href")+"/1");
-
-        Document detailstr = Jsoup.parse(detail);
-        System.out.println(detailstr.html());
-        System.out.println(detailstr.select(".main-image img").attr("src"));
-        System.out.println(detailstr.getElementsContainingText("下一»").attr("href"));
-        String src = detailstr.select(".main-image img").attr("src");
+        System.out.println(result);
+//        Document doc = Jsoup.parse(result);
+//        Elements postlist = doc.select(".postlist");
+////        Elements nav = doc.select(".nav-links");
+////        System.out.println(JsoupUtil.getCoverPageNumber(nav.html()));
+//        Elements li = postlist.select("li");
+//        li.forEach(item-> {
+//            System.out.println(item.html());
+//            System.out.println(JsoupUtil.getTitle(item.html()));
+//            System.out.println(JsoupUtil.getCoverUrl(item.html()));
+//            System.out.println(JsoupUtil.getDetailUrl(item.html()));
+//            System.out.println(JsoupUtil.getCoverDate(item.html()));
+//        });
+////        System.out.println(li.select("a").get(0).attr("href"));
+//        String detail = HttpClientUtil.get(li.select("a").get(0).attr("href")+"/1");
+////
+//        Document detailstr = Jsoup.parse(detail);
+////        System.out.println(detailstr.html());
+//        System.out.println(JsoupUtil.getDetailPageNumber(detailstr.html()));
+//        System.out.println(JsoupUtil.getDetailType(detailstr.html()));
+//        System.out.println(JsoupUtil.getDetailImgUrl(detailstr.html()));
+//        System.out.println(detailstr.select(".main-image img").first().attr("src"));
+//        System.out.println(detailstr.getElementsContainingText("下一»").attr("href"));
+//        String src = detailstr.select(".main-image img").attr("src");
 //        String img = HttpClientUtil.get(src);
 //        System.out.println(img);
-        HttpClientUtil.getImg(src);
+//        HttpClientUtil.getImg(src);
 
 
+    }
+
+    /**
+     * 爬IP池
+     */
+    @org.junit.Test
+    public void getIpPool() {
+
+        List<String> list = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect("https://www.xicidaili.com/wn/").userAgent("Mozilla").cookie("auth", "token").timeout(3000).get();
+
+            Element el = doc.getElementById("wrapper");
+            Element el2 = el.getElementById("body");
+            Elements els = el2.select("table").select("tr");
+            System.out.println("===================");
+
+            for (int i = 1; i < els.size(); i++) {
+                try {
+                    Elements tds = els.get(i).select("td");
+                    String[] ss = tds.text().split(" ");
+                    String ip = ss[0];
+                    String port = ss[1];
+                    System.out.println(ip + "=" + port);
+                    Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ss[0], Integer.parseInt(ss[1])));
+                    URLConnection httpCon = new URL("https://gz.esf.leju.com/community/f4-n3-a1442/")
+                            .openConnection(proxy);
+                    httpCon.setConnectTimeout(15000);
+                    httpCon.setReadTimeout(15000);
+                    int code = ((HttpURLConnection) httpCon).getResponseCode();
+
+                    System.out.println(code == 200);
+                    if (code == 200) {
+
+                        System.out.println("成功");
+                        list.add(ip + "=" + port);
+                    } else {
+                        System.out.println("失败");
+                    }
+                    System.out.println("===================分割线==============");
+                } catch (Exception e) {
+                    System.out.println("失败");
+                    System.out.println("===================分割线==============");
+                    continue;
+                }
+            }
+            System.out.println("===================");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("加载文档出错");
+        }
     }
 
 }
--- ldst/src/main/java/com/jayjay/ldst/util/HttpClientUtil.java
+++ ldst/src/main/java/com/jayjay/ldst/util/HttpClientUtil.java
@@ -1,7 +1,8 @@
-package com.jayjay.ldst.httpclient;
+package com.jayjay.ldst.util;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -13,6 +14,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.UUID;
 
 /**
  * Copyright © 2020-ESS
@@ -36,14 +38,17 @@ public class HttpClientUtil {
         //获取网址的返回结果
         CloseableHttpResponse response = null;
         try {
-            response = httpClient.execute(httpGet);
+                response = httpClient.execute(httpGet);
         } catch (IOException e) {
             e.printStackTrace();
+            HttpClientUtil.get(url);
         }
-
-        //获取返回结果中的实体
-        HttpEntity entity = response.getEntity();
-        String result = EntityUtils.toString(entity);
+        String result = "";
+        if(response.getStatusLine().getStatusCode() == 200) {
+            //获取返回结果中的实体
+            HttpEntity entity = response.getEntity();
+            result = EntityUtils.toString(entity);
+        }
         response.close();
         httpClient.close();
         return result;
@@ -56,7 +61,8 @@ public class HttpClientUtil {
         setConfig(httpGet);
         CloseableHttpResponse response = httpClient.execute(httpGet);
         String value = response.getEntity().getContentType().getValue().split("/")[1];
-        System.out.println(value);
+//        System.out.println(value);
+        String uuid = UUID.randomUUID().toString();
         File file = new File("D:/jayjay/jsoupimg/vvv/"+"xxx."+value);
         if(!file.getParentFile().exists()){
             file.getParentFile().mkdirs();
@@ -69,12 +75,39 @@ public class HttpClientUtil {
         return "";
     }
 
+    public static String getImg(String url,String path) throws IOException {
+        //建立一个新的请求客户端
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        setConfig(httpGet);
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        String value = response.getEntity().getContentType().getValue().split("/")[1];
+//        System.out.println(value);
+        String uuid = UUID.randomUUID().toString();
+        String fileName = uuid+"."+value;
+        File file = new File(path+fileName);
+        if(!file.getParentFile().exists()){
+            file.getParentFile().mkdirs();
+        }
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        response.getEntity().writeTo(fileOutputStream);
+        fileOutputStream.close();
+        response.close();
+        httpClient.close();
+        return fileName;
+    }
+
     public static void setConfig(HttpGet httpGet){
         httpGet.setHeader("Referer", "https://www.mzitu.com/");
-        httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36");
+        httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4270.0 Safari/537.36");
+
+        HttpHost httoHost = new HttpHost("zs-02.yuyu.cool",18910);
         httpGet.setConfig(RequestConfig.custom()
-                .setConnectionRequestTimeout(10000)
-                .setConnectTimeout(10000)
-                .setSocketTimeout(10000).build());
+                .setConnectionRequestTimeout(100000)
+                .setConnectTimeout(100000)
+                .setSocketTimeout(100000)
+                .setProxy(httoHost)
+                .build());
+
     }
 }
